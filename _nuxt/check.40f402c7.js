3b1793ff9bb314cd0d94b99fe454bffe9d6c892e
Surakit Suchaitanavanit <89428478+kimsurakit@users.noreply.github.com>
2023-01-26 02:38:36 +0700

    Add files via upload

---
+++ _nuxt/check.40f402c7.js
@@ -0,0 +1,1 @@
+import{$ as a,V as e,Z as t,a0 as r,H as u}from"./entry.7c480b0f.js";const f=a(async(n,c)=>{const{isAuth:o}=e(t()),{showModal:s}=e(r());if(!o.value)return s.value=!0,u("/")});export{f as default};
---
+++ _nuxt/composables.56891cfd.js
@@ -0,0 +1,1 @@
+import{a}from"./entry.7c480b0f.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
